e64979c7ad31f896baa5e3dfd63ba58febeab355
Andrey Kudinov <2499931@gmail.com>
2022-01-07 22:00:08 +0400

    add content

--- src/screens/BookmarkedScreen.js
+++ src/screens/BookmarkedScreen.js
@@ -1,13 +1,44 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, Button, FlatList, ScrollView } from 'react-native'
+import { HeaderButtons, Item } from 'react-navigation-header-buttons'
+import { Post } from '../components/Post'
+import { DATA } from '../data'
+import { AppHeaderIcon } from '../components/AppHeaderIcon'
 
-export const BookmarkedScreen = () => {
-  return <View style={styles.wrapper}><Text>BookmarkedScreen</Text></View>
+export const BookmarkedScreen = ({ navigation }) => {
+  const handleOpenPost = post => {
+    navigation.navigate('Post', {
+      postId: post.id,
+      date: post.date,
+      bookmarked: post.bookmarked
+    })
+  }
+
+  return (
+    <ScrollView style={styles.wrapper}>
+      <FlatList
+        data={DATA.filter(post => post.bookmarked)}
+        keyExtractor={post => post.id}
+        renderItem={({ item }) => <Post post={item} onOpen={handleOpenPost} />}
+      />
+    </ScrollView>
+  )
+}
+
+BookmarkedScreen.navigationOptions = {
+  title: 'Bookmarked',
+  headerLeft: () => (
+    <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+      <Item
+        title='Take photo'
+        iconName='ios-menu'
+        onPress={() => console.log('menu')}
+      />
+    </HeaderButtons>
+  )
 }
 
 const styles = StyleSheet.create({
   wrapper: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
+    padding: 10
   }
 })
