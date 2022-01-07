16d98c4689d4e07d0ae0865d8cb3579cf4b6189c
Andrey Kudinov <2499931@gmail.com>
2022-01-07 16:42:26 +0400

    create mock data

---
+++ src/data.js
@@ -0,0 +1,42 @@
+export const DATA = [
+  {
+    id: '1',
+    img:
+      'https://static.coindesk.com/wp-content/uploads/2019/01/shutterstock_1012724596-860x430.jpg',
+    text: 'Awesome text for post 1',
+    date: new Date().toJSON(),
+    bookmarked: true
+  },
+  {
+    id: '2',
+    img:
+      'https://cdn.londonandpartners.com/visit/general-london/areas/river/76709-640x360-houses-of-parliament-and-london-eye-on-thames-from-above-640.jpg',
+    text: 'Awesome text for post 2',
+    date: new Date().toJSON(),
+    bookmarked: true
+  },
+  {
+    id: '3',
+    img:
+      'https://static.coindesk.com/wp-content/uploads/2019/01/shutterstock_1012724596-860x430.jpg',
+    text: 'Awesome text for post 3',
+    date: new Date().toJSON(),
+    bookmarked: false
+  },
+  {
+    id: '4',
+    img:
+      'https://cdn.londonandpartners.com/visit/general-london/areas/river/76709-640x360-houses-of-parliament-and-london-eye-on-thames-from-above-640.jpg',
+    text: 'Awesome text for post 4',
+    date: new Date().toJSON(),
+    bookmarked: false
+  },
+  {
+    id: '5',
+    img:
+      'https://static.coindesk.com/wp-content/uploads/2019/01/shutterstock_1012724596-860x430.jpg',
+    text: 'Awesome text for post 5',
+    date: new Date().toJSON(),
+    bookmarked: false
+  }
+]
