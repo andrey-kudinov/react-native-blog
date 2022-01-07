dd8fcb004dc46b1e46f0446008d7277c3eb55160
Andrey Kudinov <2499931@gmail.com>
2022-01-07 19:01:37 +0400

    add content

--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -1,13 +1,59 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+  ScrollView,
+  Alert
+} from 'react-native'
+import { DATA } from '../data'
+import { THEME } from '../theme'
 
-export const PostScreen = () => {
-  return <View style={styles.wrapper}><Text>PostScreen</Text></View>
+export const PostScreen = ({ navigation }) => {
+  const postId = navigation.getParam('postId')
+
+  const post = DATA.find(post => post.id === postId)
+
+  const handleRemove = () => {
+    Alert.alert('Remove', 'Remove post', [
+      {
+        text: 'Cancel',
+        style: 'cancel'
+      },
+      { text: 'OK', style: 'destructive', onPress: () => console.log('OK Pressed') }
+    ])
+  }
+
+  return (
+    <ScrollView>
+      <Image style={styles.image} source={{ uri: post.img }} />
+      <View style={styles.textWrapper}>
+        <Text style={styles.title}>{post.text}</Text>
+      </View>
+      <Button
+        title={'Remove'}
+        color={THEME.DANGER_COLOR}
+        onPress={handleRemove}
+      />
+    </ScrollView>
+  )
+}
+
+PostScreen.navigationOptions = ({ navigation }) => {
+  const date = navigation.getParam('date')
+  return { title: `Post ${new Date(date).toLocaleDateString()}` }
 }
 
 const styles = StyleSheet.create({
-  wrapper: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
+  image: {
+    width: '100%',
+    height: 200
+  },
+  textWrapper: {
+    padding: 10
+  },
+  title: {
+    fontFamily: 'roboto'
   }
 })
