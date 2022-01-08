39baccdcdccbfeadf751f90208f31a89df70d0d0
Andrey Kudinov <2499931@gmail.com>
2022-01-08 15:35:10 +0400

    add menu

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -1,8 +1,26 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from 'react-native'
+import { HeaderButtons, Item } from 'react-navigation-header-buttons'
+import { AppHeaderIcon } from '../components/AppHeaderIcon'
 
 export const AboutScreen = () => {
-  return <View style={styles.wrapper}><Text>AboutScreen</Text></View>
+  return (
+    <View style={styles.wrapper}>
+      <Text>AboutScreen</Text>
+    </View>
+  )
 }
+
+AboutScreen.navigationOptions = ({ navigation }) => ({
+  headerLeft: () => (
+    <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+      <Item
+        title='Menu'
+        iconName='ios-menu'
+        onPress={() => navigation.toggleDrawer()}
+      />
+    </HeaderButtons>
+  )
+})
 
 const styles = StyleSheet.create({
   wrapper: {
--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,8 +1,26 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from 'react-native'
+import { HeaderButtons, Item } from 'react-navigation-header-buttons'
+import { AppHeaderIcon } from '../components/AppHeaderIcon'
 
 export const CreateScreen = () => {
-  return <View style={styles.wrapper}><Text>CreateScreen</Text></View>
+  return (
+    <View style={styles.wrapper}>
+      <Text>CreateScreen</Text>
+    </View>
+  )
 }
+
+CreateScreen.navigationOptions = ({ navigation }) => ({
+  headerLeft: () => (
+    <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+      <Item
+        title='Menu'
+        iconName='ios-menu'
+        onPress={() => navigation.toggleDrawer()}
+      />
+    </HeaderButtons>
+  )
+})
 
 const styles = StyleSheet.create({
   wrapper: {
