564edb9a2fec42237388ef072a7909bc11d9d522
Andrey Kudinov <2499931@gmail.com>
2022-01-14 22:29:21 +0400

    add data base

---
+++ src/db.js
@@ -0,0 +1,31 @@
+import * as SQLite from 'expo-sqlite'
+
+const db = SQLite.openDatabase('post.db')
+
+export class DB {
+  static init() {
+    return new Promise((resolve, reject) => {
+      db.transaction(tx => {
+        tx.executeSql(
+          'CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY NOT NULL, text TEXT NOT NULL, img TEXT, date TEXT, bookmarked INT)',
+          [],
+          resolve,
+          (_, error) => reject(error)
+        )
+      })
+    })
+  }
+
+  static getPosts() {
+    return new Promise((resolve, reject) => {
+      db.transaction(tx => {
+        tx.executeSql(
+          'SELECT * FROM posts',
+          [],
+          (_, result) => resolve(result.rows._array),
+          (_, error) => reject(error)
+        )
+      })
+    })
+  }
+}
--- src/fonts.js
+++ src/fonts.js
@@ -1,8 +1,16 @@
 import * as Font from 'expo-font'
+import { DB } from './db'
 
 export const fonts = async () => {
-  await Font.loadAsync({
-    'roboto': require('../assets/fonts/Roboto-Regular.ttf'),
-    'roboto-bold': require('../assets/fonts/Roboto-Bold.ttf')
-  })
+  try {
+    await Font.loadAsync({
+      roboto: require('../assets/fonts/Roboto-Regular.ttf'),
+      'roboto-bold': require('../assets/fonts/Roboto-Bold.ttf')
+    })
+
+    await DB.init()
+    console.log('Database starting...')
+  } catch (error) {
+    console.log('Error -', error)
+  }
 }
