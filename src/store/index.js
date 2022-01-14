5ea4d26cd4064fd43718450904b5e3af0078a63a
Andrey Kudinov <2499931@gmail.com>
2022-01-14 22:28:53 +0400

    add redux-thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
-import { combineReducers, createStore } from 'redux'
+import { applyMiddleware, combineReducers, createStore } from 'redux'
+import thunk from 'redux-thunk'
 import { postReducer } from './reducers/postReducer'
 
 const rootReducer = combineReducers({ blog: postReducer })
 
-export default createStore(rootReducer)
+export default createStore(rootReducer, applyMiddleware(thunk))
