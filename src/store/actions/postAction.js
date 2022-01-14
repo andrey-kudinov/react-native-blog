cbd5c5f60c6685a13232507d88ba94a87fe75dc6
Andrey Kudinov <2499931@gmail.com>
2022-01-14 22:29:35 +0400

    add load posts

--- src/store/actions/postAction.js
+++ src/store/actions/postAction.js
@@ -1,9 +1,11 @@
-import { LOAD_POSTS, REMOVE_POST, TOGGLE_BOOKMARKED, ADD_POST } from "../types"
+import { LOAD_POSTS, REMOVE_POST, TOGGLE_BOOKMARKED, ADD_POST } from '../types'
+import { DB } from '../../db'
 
 export const loadPosts = () => {
-  return {
-    type: LOAD_POSTS,
-    payload: []
+  return async dispatch => {
+    const posts = await DB.getPosts()
+    
+    dispatch({ type: LOAD_POSTS, payload: [] })
   }
 }
 
